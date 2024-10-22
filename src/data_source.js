3a56cead0c7c2e93a49cad10be5a19c4bf3d3bf8
chiappettafran <franciscomartinezchiappetta@gmail.com>
2024-10-22 20:57:21 -0300

    configuración deploy

--- src/data_source.js
+++ src/data_source.js
@@ -4,7 +4,7 @@ import { User } from "./entities/User.js";
 import { Invoice } from "./entities/Invoice.js";
 import { Invoice_Detail } from "./entities/Invoice_Detail.js";
 import { Product } from "./entities/Product.js";
-import {Category} from "./entities/Category.js";
+import { Category } from "./entities/Category.js";
 
 export const AppDataSource = new DataSource({
     type: "better-sqlite3",
